f465cdfe4f2f33935269506d81300d5190db242c
Marshall Farrier <info@marshallfarrier.com>
2013-10-27 01:21:27 -0700

    Initial commit.

---
+++ custom/scripts/views/app-view.js
@@ -0,0 +1,21 @@
+var codeMelon = codeMelon || {}; 
+codeMelon.games = codeMelon.games || {}; 
+
+codeMelon.games.AppView = Backbone.View.extend({
+    initialize: function() {
+        var _this = this;
+
+        _.bindAll(_this,
+            'render'
+        );
+
+        _this.render();
+    },
+
+    render: function() {
+        var _this = this,
+            content = '<p>JavaScript generated content</p>';
+
+        _this.$el.find('.col-md-12').append(content);
+    }
+});
